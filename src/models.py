e294a7e42f7a2ee62c22dade65c4db560304fd87
JDigar <jdigar96@gmail.com>
2022-06-23 08:51:07 +0000

    done

--- src/models.py
+++ src/models.py
@@ -17,3 +17,61 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    age = Column(String(120), nullable=False)
+    email = Column(String(120), nullable=False)
+    favorites = relationship('Favorites', backref='user', lazy=True)
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    population = Column(String(120), nullable=False)
+    rotation = Column(String(120), nullable=False)  
+    diameter = Column(String(120), nullable=False)   
+    favorites = relationship('Favorites', backref='planets', lazy=True)
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    gender = Column(String(120), nullable=False)
+    height = Column(String(120), nullable=False)  
+    eye_color = Column(String(120), nullable=False)   
+    favoritos = relationship('Favoritos', backref='characters', lazy=True)
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    model = Column(String(120), nullable=False)
+    manufacturer = Column(String(120), nullable=False)  
+    Cost_in_credits = Column(String(120), nullable=False)   
+    favoritos = relationship('Favoritos', backref='vehicles', lazy=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),
+        nullable=False)
+    planets_id = Column(Integer, ForeignKey('planets.id'),
+        nullable=False)
+    charancteres_id = Column(Integer, ForeignKey('characters.id'),
+        nullable=False)
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'),
+        nullable=False)
+
+    def to_dict(self):
+        return {}
+
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
